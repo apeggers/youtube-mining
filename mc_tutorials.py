78d8f076aade5d0f3200cca47215b97c342b093c
Alexander Eggers <apeggers97@gmail.com>
2020-10-30 01:20:11 -0500

    More data, better charts

--- mc_tutorials.py
+++ mc_tutorials.py
@@ -1,3 +1,6 @@
+import os
+
+import socket
 import json
 import datetime as dt
 import isodate
@@ -7,49 +10,70 @@ import matplotlib.pyplot as plt
 import googleapiclient.discovery
 import googleapiclient.errors
 
+from dotenv import load_dotenv
+load_dotenv()
+
+socket.setdefaulttimeout(10)
 
 def main():
     api_service_name = "youtube"
     api_version = "v3"
-    api_key = "AIzaSyA01YcUhlrZKc72Adeh2hsBAg_GCM5ozZM"
-
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+    API_KEY = os.getenv("API_KEY")
     
+    # Initialize API connection
+    print("Connecting to Google API...")
+    try:
+        youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=API_KEY)
+    except socket.timeout:
+        print("Connection to Google API timed out.\nEXITING")
+        return
+    else:
+        print("SUCCESS")
+
     page = None
     video_durations = []
-    while(true):
-        # Get page of search results
-        request = youtube.search().list(
-            part="snippet",
-            q="minecraft tutorial",
-            maxResults=50,
-            publishedAfter="2015-10-29T00:00:00Z",
-            type="video",
-            pageToken=page
-        )
-        search_results = request.execute()
-    
-        # Extract video id's from search results
-        video_ids = []
-        for search_result in search_results["items"]:
-            video_ids.append(search_result["id"]["videoId"])
-    
-        # Get video durations based on video id's
-        request = youtube.videos().list(
-            part="contentDetails",
-            id=",".join(video_ids)
-        )
-        videos = request.execute()
 
-        for video in videos["items"]:
-            duration = video["contentDetails"]["duration"]
-            video_durations.append(isodate.parse_duration(duration).seconds)
+    # Loop through each year to maximize results
+    for year in range(2016, 2021, 2):
+
+        # API gives 50-item pages so must loop through each one
+        while(True):
+            # Get page of search results
+            request = youtube.search().list(
+                part="snippet",
+                q="minecraft tutorial",
+                maxResults=50,
+                publishedAfter=str(year)+"-1-1T00:00:00Z",
+                publishedBefore=str(year+1)+"-1-1T00:00:00Z",
+                type="video",
+                pageToken=page
+            )
+            search_results = request.execute()
         
-        if "nextPageToken" in search_results:
-            page = search_results["nextPageToken"]
-            print(page)
-        else:
-            break
+            # Extract video id's from search results
+            video_ids = []
+            for search_result in search_results["items"]:
+                video_ids.append(search_result["id"]["videoId"])
+        
+            # Get video durations based on video id's
+            request = youtube.videos().list(
+                part="contentDetails",
+                id=",".join(video_ids)
+            )
+            videos = request.execute()
+
+            for video in videos["items"]:
+                duration = video["contentDetails"]["duration"]
+                video_durations.append(isodate.parse_duration(duration).seconds)
+            
+            if "nextPageToken" in search_results:
+                page = search_results["nextPageToken"]
+            else:
+                page = None
+                break
+    
+    # Remove 0-duration videos
+    list(filter((0).__ne__, video_durations))
 
     # Print basic stats
     n = len(video_durations)
@@ -62,10 +86,15 @@ def main():
     # Plot histogram
     dur_range = max(video_durations)-min(video_durations)
     bin_size = 60 #seconds
-    plt.hist(video_durations, color = 'blue', edgecolor = 'black', bins = int(dur_range/bin_size))
+    
+    sns.distplot(video_durations, hist=True, kde=True,
+                 bins = int(dur_range/bin_size), color = 'darkblue',
+                 hist_kws={'edgecolor':'black'}, kde_kws={'linewidth': 4})
+
     plt.title('Duration of Videos')
     plt.xlabel('Seconds')
-    plt.ylabel('Videos')
+    plt.ylabel('Density')
+    
     plt.show()
 
 def split_list(l, n):
